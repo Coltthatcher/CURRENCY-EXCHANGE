542157da0bb16343979792cd7f233f8416af2471
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-19 11:15:15 -0800

    WIP: decifering API KEY

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CalculateRate from './calculaterate.js';
+import CurrencyExchange from './Currency.js';
+
+function  getElements(response) {
+  if (response) {
+    const currencyIndex
+  }
+}
+
