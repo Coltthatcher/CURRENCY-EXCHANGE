57e057fcd98386d8fc827831355c70b64e89cce4
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-19 10:51:31 -0800

    Add static async function getCash

--- src/Currency.js
+++ src/Currency.js
@@ -1,3 +1,13 @@
 export default class CurrencyExchange {
-  static async 
+  static async getCash() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
 }
