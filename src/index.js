6eafd3c9b5df4d27a6da11bfd3daffa1e51e84ca
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-19 16:00:03 -0800

    WIP:rethinking entire structure

--- src/index.js
+++ src/index.js
@@ -2,51 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import CalculateRate from './calculaterate.js';
 import CurrencyExchange from './Currency.js';
 
-/*function getSelectedClass(response) {
-  new CalculateRate(response.CalculateRate("#usdInput").val(),
-  (".showRate")('#currencies option:selected').val());
-}*/
-
-function getElements(response) {
-  if (response) {
-    let currencyIndex= $('#currencies').find(":selected").val();
-
-    console.log(currencyIndex)
-    // const selectedCurrency = getSelectedClass(response);
-    $('.showRate').html(/*selectedCurrency.getInfo()*/);
-    } else {
-    $('.showErrors').text(`There was an error: ${response}`);
-  }
-}
-/*CalculateRate(usdAmount){
-  return(usdAmount / this.exchange.toFixed(9));
-}
-getInfo(){
-  return `The exchange for ${this.exchange} to this 1 USD`
-}*/
 
 
-async function makeApiCall() {
-  const response = await CurrencyExchange.getCash();
-  getElements(response);
-}
-
-async function getMenu() {
-  const response = await CurrencyExchange.getCash();
-  if (response) {
-    menuLoop(response);
-  }else{
-    $('.showErrors').text(`there was an error: ${response}`);
-  }
-}
 
 function menuLoop(response) {
   const obj = response.conversion_rates
-  Object.keys(obj).forEach((key,i) => {
-    let menuItem = `<option value="${i}">${key}</option>`;
+  Object.keys(obj).forEach((key) => {
+    let menuItem = `<option value="${key}">${key}</option>`;
     $("#currencies").append(`${menuItem}`);
     console.log(menuItem)
   });
