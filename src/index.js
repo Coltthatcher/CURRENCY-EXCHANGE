c32bfd3981ab6d410bc203e1766202daeec8417c
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-19 18:08:29 -0800

    WIP: broke menuLoop when changing calculateexchange

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import CurrencyExchange from './Currency.js';
 
 function calculateExchange(response, key) {
   if (response.conversion_rates) {
-    if(isNaN(response.conversion_rates[`${key}`])) {
+    if(!(response.conversion_rates[`${key}`])) {
       return $('.showErrors').text("Sorry, we couldn't convert that for you!");
     }
     $('.showRate').html(`${key}`/response.conversion_rates`${key}`);
@@ -21,7 +21,6 @@ function menuLoop(response) {
   Object.keys(obj).forEach((key) => {
     let menuItem = `<option value="${key}">${key}</option>`;
     $("#currencies").append(`${menuItem}`);
-    console.log(menuItem)
   });
 }
 
