76548d42b1324fb376b4d083b6933dc35f8dc3bc
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-19 16:19:02 -0800

    rearrange calculateExchange to display input on page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './Currency.js';
 
+function calculateExchange(response, USD, newCountry) {
+  if (response.conversion_rates) {
+    if(isNaN(response.conversion_rates[`${newCountry}`])) {
 
+    }
+  }
+}
 
 
 function menuLoop(response) {
