c6782f91b4097a5bbea6f5c3d0a6ed92abdd6cea
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-19 18:30:32 -0800

    WIP:MenuLoop no longer working

--- src/Currency.js
+++ src/Currency.js
@@ -2,7 +2,7 @@ export default class CurrencyExchange {
   static async getCash() {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-      if (!response.ok) {
+      if (response.ok) {
         throw Error(response.statusText);
       }
       return response.json();
